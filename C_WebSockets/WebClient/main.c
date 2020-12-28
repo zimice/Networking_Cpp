c7afa932f67f97f153eb9a0fbda403716557a780
zimice <simon.kochanek@me.com>
2020-12-28 23:05:57 +0100

    Reseting this repository

---
+++ C_WebSockets/WebClient/main.c
@@ -0,0 +1,1 @@
+#inlude <sys/socket.h>
---
+++ Javidx9_Part1/Windows_Visual/Javidsx9_Networking/Javidsx9_Networking/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+int main() {
+    asio::error_code ec;
+
+    asio::io_context context; //create a "context" -essentially the platform specific interface
+
+    //Get the address of something to connect to 
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if (ec) {
+        std::cout << "Error" << std::endl;
+    }
+    else {
+        std::cout << "Successfull" << std::endl;
+    }
+
+
+    return 0;
+}
